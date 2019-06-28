9caaffcfe7174c8a1a5132372b5484e05e0752e5
Kevin Steele <kevdsteele@gmail.com>
2019-06-28 11:30:05 -0400

    logic update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -80,6 +80,7 @@ var breweries=[];
        var mediaDiv=$("<div>");
        mediaDiv.addClass("media");
        mediaDiv.attr("id", "media"+i);
+       mediaDiv.atrr("value", i);
 
        $("#results").append(mediaDiv);
 
@@ -177,7 +178,7 @@ var breweries=[];
     
             function createMarker(loc,brewName,brewAdd, brewPh, label, brewRat) { 
             
-            var marker = new google.maps.Marker({position: loc, label: label , map: map});
+            var marker = new google.maps.Marker({position: loc, label: label , map: map, value:i});
        
            
             google.maps.event.addListener(marker, 'click', function() {
