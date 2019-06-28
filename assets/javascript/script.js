a4de1406c1f88d3cc165b7817519ed6ebea286a2
Kevin Steele <kevdsteele@gmail.com>
2019-06-27 22:38:01 -0400

    Layout finished need pagination

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,9 +2,7 @@
 $(document).ready(function () {
 
     
-        $('.listing-block').slimScroll({
-            height: '500px'
-        });
+       
 
 
 var breweries=[];
@@ -13,31 +11,30 @@ var breweries=[];
     
         var map = new google.maps.Map(document.getElementById('gmap'), {zoom: 12, center: mainloc}); 
 
-$.validator.setDefaults( {
-    submitHandler: function () {
+
     
-        map = new google.maps.Map(document.getElementById('gmap'), {zoom: 12, center: mainloc}); 
+        map = new google.maps.Map(document.getElementById('gmap'), {zoom: 11, center: mainloc}); 
         $("#zip-error").empty();    
-        var zip;
-        var radius=0;
-        var rating=0;
+        var zip=20009;
+        var radius=15000;
+        var rating=3;
         breweries=[];
         
         
-        console.log("Search clicked")
+       /* console.log("Search clicked")
         zip = $("#zip").val().trim();
         radius =$("#radius").val().trim();
         rating =$("#rating").val().trim();
     
         console.log("zip is " + zip)
-        console.log("radius is " + radius)
+        console.log("radius is " + radius)*/
     
       
     
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + zip + "&term=breweries&categories=breweries&radius=" + radius +"&rating=4&limit=50&sort_by=distance",
+            "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + zip + "&term=breweries&categories=breweries&radius=" + radius +"&rating=4&limit=10&sort_by=distance",
             "method": "GET",
             "headers": {
               "Authorization": "Bearer 8D_mZteQabQeW-jEZAK4kU4o9h7PhhECcqPsritDt99eippSSN851BkePtOuCLpVShTshzeKUUKDiHj51cX4vJMN0YZY_tPNJVTsapTBgoWt0dErzhHH1psW0FYKXXYx",
@@ -79,9 +76,95 @@ $.validator.setDefaults( {
         
          for (i=0; i < breweries.length; i++) {
     
+/*dom push*/
+       var mediaDiv=$("<div>");
+       mediaDiv.addClass("media");
+       mediaDiv.attr("id", "media"+i);
+
+       $("#results").append(mediaDiv);
+
+
+       var mediaImg=$("<img>");
+       mediaImg.attr("id", "media-image"+i);
+       mediaImg.attr("src", breweries[i].image_url);
+       mediaImg.addClass("d-flex align-self-start");
+
+       $("#media"+i).append(mediaImg);
+
+       var bodyDiv=$("<div>");
+       bodyDiv.addClass("media-body pl-3");
+       bodyDiv.attr("id", "media-body"+i);
+
+       $("#media"+i).append(bodyDiv);
+
+       var bodyHeadDiv=$("<div>");
+       bodyHeadDiv.addClass("stats");
+       bodyHeadDiv.attr("id", "head-div"+i);
+
+       
+
+      
+
+       var barNameSpan=$("<span>");
+       barNameSpan.attr("id", "bar-name"+i);
+       barNameSpan.text((i+1) + ". " + breweries[i].name);
+
+      
+
+       var barAdd1Div=$("<div>");
+       barAdd1Div.attr("id", "bar-add-1"+i)
+       barAdd1Div.addClass("address");
+       barAdd1Div.text(breweries[i].location.display_address[0]);
+
+       var barAdd2Div=$("<div>");
+       barAdd2Div.attr("id", "bar-add-2" +i);
+       barAdd2Div.addClass('address');
+       barAdd2Div.text(breweries[i].location.city + " " + breweries[i].location.zip_code);
+
+       var barPhDiv = $("<div>");
+       barPhDiv.attr("id", "bar-phone"+i);
+       barPhDiv.addClass("address");
+       barPhDiv.text(breweries[i].display_phone);
+
+        var barRatSpan=$("<span>");
+       barRatSpan.attr("id", "bar-rat"+i);
+       barRatSpan.text("Rating ");
+       barRatSpan.addClass("rat-span");
+       
+
+       var barYelpWeb =$("<div>");
+       barYelpWeb.attr("id", "yelp-web"+i);
+       barYelpWeb.addClass("yelplink");
+       barYelpWeb.html('<a target="_blank" href="' + breweries[i].url + '"> Yelp Website</a>');
+
+        var barStarOuterDiv = $("<div>");
+        barStarOuterDiv.addClass("stars-outer");
+        barStarOuterDiv.attr("id", "stars-outer"+i);
+
+        var barRat = parseInt(breweries[i].rating);
+        var ratPerc = ((barRat/5) * 100);
+
+        var barStarInnerDiv = $("<div>");
+        barStarInnerDiv.addClass("stars-inner");
+        barStarInnerDiv.attr("id", "stars-inner"+i);
+        barStarInnerDiv.css("width", ratPerc+"%");
+
+
+
+
+
+
+
+       $("#media-body"+i).append(bodyHeadDiv, barAdd1Div, barAdd2Div, barPhDiv, barYelpWeb);
+       $("#head-div"+i).append(barNameSpan);
+       $("#yelp-web"+i).append(barRatSpan);
+       $("#bar-rat"+i).append(barStarOuterDiv);
+       $("#stars-outer"+i).append(barStarInnerDiv);
+
+
             
             
-    
+    /* GoogleMap section*/
             var loc = {lat: breweries[i].coordinates.latitude, lng: breweries[i].coordinates.longitude}
             var brewName = breweries[i].name;
             var brewAdd = breweries[i].location.address1 + " " + breweries[i].location.city + " " + breweries[i].location.zip_code;
@@ -111,34 +194,9 @@ $.validator.setDefaults( {
 
 
 
-    }
-    } );
     
-    $( document ).ready( function () {
-    $( "#search-form" ).validate( {
-    rules: {
-    zip: "required"
-    },
-    messages: {
-    zip: "Please enter a zip code",
-    },
-    errorElement: "em",
-    errorPlacement: function ( error, element ) {
-    // Add the `help-block` class to the error element
-    error.addClass( "help-block" );
-    
-    error.insertAfter( "#message");
-    
-    },
-    highlight: function ( element, errorClass, validClass ) {
-    $( element ).parents( ".zip-code" ).addClass( "has-error" ).removeClass( "has-success" );
-    },
-    unhighlight: function (element, errorClass, validClass) {
-    $( element ).parents( ".zip-code" ).addClass( "has-success" ).removeClass( "has-error" );
-    }
-    } );
+   
     
     
-    } );
 
 });
